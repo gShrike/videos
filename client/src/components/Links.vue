2a6bce2dd6aa348dde577012bc29c3163440fee1
Roberto Ortega <berto.ort@gmail.com>
2017-12-03 17:42:32 -0700

    💁‍♀️ add tag styling

--- client/src/components/Links.vue
+++ client/src/components/Links.vue
@@ -5,10 +5,16 @@
       <input class="search" type="text"/>
       <h2 v-if="loading">Loading...</h2>
       <h2 v-if="error">Oh no! We couldn't load the links. Try again later</h2>
-      <section class="links" v-for="link in links">
-        <h5><a :href="link.url">{{link.title}}</a></h5>
-        <iframe  v-if="link.embed" width="560" height="315" :src="link.embed" frameborder="0" allowfullscreen></iframe>
-      </section>
+      <ul class="links" v-for="link in links">
+        <li>
+          <h2><a :href="link.url">{{link.title}}</a></h2>
+          <input type="button" class="show" v-on:click="toggleView(link)" v-if="link.embed" value="View" />
+          <ul class="tags">
+            <li v-for="tag in link.tags" class="tag">{{tag.name}}</li>
+          </ul>
+          <iframe  v-if="link.embed && link.show" width="560" height="315" :src="link.embed" frameborder="0" allowfullscreen></iframe>
+        </li>
+      </ul>
     </div>
   </main>
 </template>
@@ -35,6 +41,11 @@ export default {
         this.loading = false;
         this.error = true;
       });
+  },
+  methods: {
+    toggleView(link) {
+      this.$set(link, 'show', !link.show);
+    },
   },
 };
 </script>
